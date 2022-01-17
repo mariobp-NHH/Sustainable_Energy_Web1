1e16576b1e73ca14c42eb0280dd0315dc2ae46be
Gabriel Fuentes <45942967+gabrielfuenmar@users.noreply.github.com>
2022-01-17 22:50:39 +0100

    Update __init__.py

--- webse/__init__.py
+++ webse/__init__.py
@@ -9,7 +9,7 @@ DB_VAR=os.environ.get('DATABASE_URL_POSTGRESQL', None)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_VAR
+app.config['SQLALCHEMY_DATABASE_URI'] ="sqlite:///site.db"
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
