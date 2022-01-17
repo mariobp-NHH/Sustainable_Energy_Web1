12110893684d610ca8562c0c7ce7e255b30fe0b6
Gabriel Fuentes <45942967+gabrielfuenmar@users.noreply.github.com>
2022-01-17 22:36:14 +0100

    Update __init__.py

--- webse/__init__.py
+++ webse/__init__.py
@@ -4,12 +4,12 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
 
-# DB_VAR=os.environ.get('DATABASE_URL_POSTGRESQL', None)
+DB_VAR=os.environ.get('DATABASE_URL_POSTGRESQL', None)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nwnuqchwcpcjss:7176f206a90a4161a0b7a4d1d74eb6c5f1c4382be315c7cc816cd9e034511f8c@ec2-52-31-217-108.eu-west-1.compute.amazonaws.com:5432/de5cnke41h2r55'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_VAR
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
