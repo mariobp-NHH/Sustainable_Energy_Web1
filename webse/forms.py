dbf7e5887ec21dc1c206e7b62515122e0644d07b
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2022-01-17 14:17:43 +0100

    Add files via upload

--- webse/forms.py
+++ webse/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField, RadioField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, InputRequired
 from webse.models import User
 
 
@@ -39,6 +39,10 @@ class UpdateAccountForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+    group = SelectField('Group', validators=[DataRequired()],
+                       choices=[('0', '0'),
+                                ('1', '1'),
+                                ('2', '2')])
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
@@ -57,8 +61,107 @@ class UpdateAccountForm(FlaskForm):
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
+    submit = SubmitField('Chat-Post')
 
-class PostModuls(FlaskForm):
-    module1_q1_string = StringField('Module 1 Q1', validators=[DataRequired()])
-    submit = SubmitField('Check')
+class AnnouncementForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Annoucement')
+
+#App Statistics
+class AppStatisticsForm(FlaskForm):
+    type = SelectField(validators=[DataRequired()],
+                        choices=[('Ch1. Introduction', 'Ch1. Introduction'),
+                                 ('Ch2. Installation', 'Ch2. Installation')])
+    submit = SubmitField('App Module Statistics')
+
+#Sustainable Energy Statistics
+class SEStatisticsForm(FlaskForm):
+    type = SelectField(validators=[DataRequired()],
+                        choices=[('Ch1. Overview', 'Ch1. Overview'),
+                                 ('Ch2. Wind', 'Ch2. Wind')])
+    submit = SubmitField('Sustainable Energy Module Statistics')
+
+#M1_Ch1
+class ModulsForm_M1_Ch1_Q1(FlaskForm):
+    type = SelectField(validators=[DataRequired()],
+                       choices=[('Python', 'Python'),
+                                ('HTML', 'HTML')])
+    submit = SubmitField('Programming Language')
+
+class ModulsForm_M1_Ch1_Q2(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('GitHub', 'GitHub'),
+                                ('Heroku', 'Heroku')])
+    submit = SubmitField('Server')
+
+class ModulsForm_M1_Ch1_Q3(FlaskForm):
+    type = RadioField(choices=['Easy', 'Medium', 'Difficult'],
+                       validators=[InputRequired()])
+    submit = SubmitField('Implementation')
+
+#M1_Ch2
+class ModulsForm_M1_Ch2_Q1(FlaskForm):
+    identifier = StringField()
+    question_str = StringField('App Module, Chapter 2, Question 1', validators=[DataRequired()])
+    submit1 = SubmitField('Check')
+
+class ModulsForm_M1_Ch2_Q2(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('wind', 'wind'),
+                                ('solar', 'solar')])
+    submit = SubmitField('Type Energy')
+
+class ModulsForm_M1_Ch2_Q3(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                                choices=[('income', 'income'),
+                                        ('expense', 'expense')])
+    submit = SubmitField('Type Income')
+
+class ModulsForm_M1_Ch2_Q4(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                                choices=[('university', 'university'),
+                                        ('school', 'school')])
+    submit = SubmitField('Type Work')
+
+class ModulsForm_M1_Ch2_Q5(FlaskForm):
+    type = RadioField('Level',
+                       choices=['Petrol', 'Electric', 'Hydrogen'],
+                       validators=[InputRequired()])
+    submit = SubmitField('Type type')
+
+#M2_Ch1
+class ModulsForm_M2_Ch1_Q1(FlaskForm):
+    type = SelectField(validators=[DataRequired()],
+                       choices=[('Europe', 'Europe'),
+                                ('Africa', 'Africa')])
+    submit = SubmitField('Continent')
+
+class ModulsForm_M2_Ch1_Q2(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('Local', 'Local'),
+                                ('National', 'National')])
+    submit = SubmitField('Geographical Level')
+
+class ModulsForm_M2_Ch1_Q3(FlaskForm):
+    type = RadioField(choices=['Easy', 'Medium', 'Difficult'],
+                       validators=[InputRequired()])
+    submit = SubmitField('Implementation')
+
+#M2_Ch2
+class ModulsForm_M2_Ch2_Q1(FlaskForm):
+    type = SelectField(validators=[DataRequired()],
+                       choices=[('Europe', 'Europe'),
+                                ('Africa', 'Africa')])
+    submit = SubmitField('Continent')
+
+class ModulsForm_M2_Ch2_Q2(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('Local', 'Local'),
+                                ('National', 'National')])
+    submit = SubmitField('Geographical Level')
+
+class ModulsForm_M2_Ch2_Q3(FlaskForm):
+    type = RadioField(choices=['Easy', 'Medium', 'Difficult'],
+                       validators=[InputRequired()])
+    submit = SubmitField('Implementation')
--- webse/models.py
+++ webse/models.py
@@ -10,10 +10,14 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
+    group = db.Column(db.Integer, unique=False, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    moduls = db.relationship('Moduls', backref='author', lazy=True)
+    announcements = db.relationship('Announcement', backref='author', lazy=True)
+    postsg = db.relationship('PostG', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -29,13 +33,37 @@ class Post(db.Model):
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
 
-class Moduls(db.Model):
+class PostG(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    module1_q1_string = db.Column(db.String(100))
-    module1_q1_number = db.Column(db.Integer)
+    title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    group = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Moduls('{self.module1_q1_string}', '{self.module1_q1_number}', '{self.date_posted}')"
+        return f"PostG('{self.title}', '{self.date_posted}')"
+
+class Announcement(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Announcement('{self.title}', '{self.date_posted}')"
+
+class Moduls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title_mo = db.Column(db.String(100), nullable=False)
+    title_ch = db.Column(db.String(100), nullable=False)
+    question_num = db.Column(db.Integer)
+    question_str = db.Column(db.String(100))
+    question_result = db.Column(db.Integer)
+    date_exercise = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Moduls('{self.question_str}', '{self.question_result}', '{self.date_exercise}')"
 
