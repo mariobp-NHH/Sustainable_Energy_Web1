9549850c5a7bf0c20aeab243465c199228123c54
Gabriel Fuentes <45942967+gabrielfuenmar@users.noreply.github.com>
2022-01-31 14:13:46 +0100

    Update models.py

--- webse/models.py
+++ webse/models.py
@@ -9,7 +9,7 @@ def load_user(user_id):
 
  
 class Userpage(db.Model, UserMixin):
-    __tablename__="userpage
+    __tablename__="userpage"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
