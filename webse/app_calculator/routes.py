18181701062d179532e836aa7483e674ef896daf
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2022-02-04 16:40:58 +0100

    Update routes.py

--- webse/app_calculator/routes.py
+++ webse/app_calculator/routes.py
@@ -16,29 +16,26 @@ app_calculator = Blueprint('app_calculator', __name__)
 #####################################
 
 ##Emissions factor per transport in kg per passemger km
+##Emissions factor per transport in kg per passemger km
 ##++++++++++++++++++++++
 efco2={"Bus":{"Diesel":0.10231,"CNG":0.08,"Petrol":0.10231,"No Fossil Fuel":0},
-       "Car":{"Petrol":0.18592,"Diesel":0.16453,"No Fossil Fuel":0},
-       "Plane":{"No Fossil Fuel":0.24298},
-       "Ferry":{"Diesel":0.11131, "No Fossil Fuel":0},
+       "Car":{"Hybrid":0.10567,"Petrol":0.18592,"Diesel":0.16453,"No Fossil Fuel":0},
+       "Plane":{"Jet Fuel":0.24298,"No Fossil Fuel":0},
+       "Ferry":{"Diesel":0.11131,"HFO":0.1131,"No Fossil Fuel":0},
        "Motorbike":{"Petrol":0.09816,"No Fossil Fuel":0},
        "Scooter":{"No Fossil Fuel":0},
        "Bicycle":{"No Fossil Fuel":0},
        "Walk":{"No Fossil Fuel":0}}
-#"Ferry":{"Diesel":0.11131,"HFO":0.1131,"No Fossil Fuel":0},
-#"Car":{"Hybrid":0.10567,"Petrol":0.18592,"Diesel":0.16453,"No Fossil Fuel":0},
-#"Plane":{"Jet-Fuel":0.24298,"No Fossil Fuel":0},
+
 efch4={"Bus":{"Diesel":2e-5,"CNG":2.5e-3,"Petrol":2e-5,"No Fossil Fuel":0},
-       "Car":{"Petrol":3.1e-4,"Diesel":3e-6,"No Fossil Fuel":0},
-       "Plane":{"No Fossil Fuel":1.1e-4},
-       "Ferry":{"Diesel":3e-5,"No Fossil Fuel":0},
+       "Car":{"Hybrid":1.5e-4,"Petrol":3.1e-4,"Diesel":3e-6,"No Fossil Fuel":0},
+       "Plane":{"Jet Fuel":1.1e-4,"No Fossil Fuel":0},
+       "Ferry":{"DO":3e-5,"HFO":3e-5,"No Fossil Fuel":0},
        "Motorbike":{"Petrol":2.1e-3,"No Fossil Fuel":0},
        "Scooter":{"No Fossil Fuel":0},
        "Bicycle":{"No Fossil Fuel":0},
        "Walk":{"No Fossil Fuel":0}}
-#"Ferry":{"Diesel":3e-5,"HFO":3e-5,"No Fossil Fuel":0},
-#"Car":{"Hybrid":1.5e-4,"Petrol":3.1e-4,"Diesel":3e-6,"No Fossil Fuel":0},
-#"Plane":{"Jet-Fuel":1.1e-4,"No Fossil Fuel":0},
+
 #+++++++++++++++++++++++
 
 @app_calculator.route("/app_calculator", methods=['GET', 'POST'])
